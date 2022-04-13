cbf51571ad804fef7b77bd935518ef4ff1e313a3
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-04-13 08:37:44 -0400

    added login button

--- app/src/main/java/com/nirmalbhetwal/androidfinalexam/MainActivity.java
+++ app/src/main/java/com/nirmalbhetwal/androidfinalexam/MainActivity.java
@@ -3,12 +3,30 @@ package com.nirmalbhetwal.androidfinalexam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText etUsername, etPassword;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize the edit texts
+        etUsername = (EditText) findViewById(R.id.editTextUserName);
+        etPassword = (EditText) findViewById(R.id.editTextPassword);
+        // initialize the login button
+        loginButton = (Button) findViewById(R.id.btnLogin);
+
+        loginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        
     }
 }
